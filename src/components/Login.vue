9706e7af1e532329e64e84473a1f341b90d7a167
zawthuphyo <ztphyo101@gmail.com>
2025-04-04 22:16:51 +0700

    signup and login form

---
+++ src/components/Login.vue
@@ -0,0 +1,31 @@
+<template>
+  <h2>Log In</h2>
+  <form @submit.prevent="logIn">
+    <input type="email" placeholder="Email" required v-model="email" />
+    <input type="password" placeholder="Password" required v-model="password" />
+    <button>Log In</button>
+  </form>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const email = ref("");
+    const password = ref("");
+
+    let logIn = async () => {
+      console.log(email.value, password.value);
+    };
+
+    return {
+      email,
+      password,
+      logIn,
+    };
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/Signup.vue
@@ -0,0 +1,34 @@
+<template>
+  <h2>Sign Up</h2>
+  <form @submit.prevent="signUp">
+    <input type="text" placeholder="Username" required v-model="displayName" />
+    <input type="email" placeholder="Email" required v-model="email" />
+    <input type="password" placeholder="Password" required v-model="password" />
+    <button>Sign Up</button>
+  </form>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const displayName = ref("");
+    const email = ref("");
+    const password = ref("");
+
+    let signUp = async () => {
+      console.log(displayName.value, email.value, password.value);
+    };
+
+    return {
+      displayName,
+      email,
+      password,
+      signUp,
+    };
+  },
+};
+</script>
+
+<style></style>
