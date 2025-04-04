2cc447f0fb38edfaf033029da929d056b1eeb28d
zawthuphyo <ztphyo101@gmail.com>
2025-04-04 22:03:03 +0700

    firebase setup

---
+++ src/firebase/config.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAXPogmJhceb6qKsTA8UEM1cX9MLAQAkeU",
+  authDomain: "vue-blog-system-bc26c.firebaseapp.com",
+  projectId: "vue-blog-system-bc26c",
+  storageBucket: "vue-blog-system-bc26c.firebasestorage.app",
+  messagingSenderId: "992590442389",
+  appId: "1:992590442389:web:205924ba5b6ab7e03e909c",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+let db = firebase.firestore();
+let timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { db, timestamp };
