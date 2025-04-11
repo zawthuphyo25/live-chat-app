c45fb1ec0be74b57e617dec9bb335aa9091414ca
zawthuphyo <ztphyo101@gmail.com>
2025-04-11 23:34:26 +0700

    watching the  current user

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -5,9 +5,24 @@
 </template>
 
 <script>
+import { watch } from "vue";
 import Navbar from "../components/Navbar";
+import getUser from "@/composables/getUser";
+import { useRouter } from "vue-router";
+
 export default {
   components: { Navbar },
+  setup() {
+    let { user } = getUser();
+    let router = useRouter();
+
+    watch(user, () => {
+      // console.log(user.value);
+      if (!user.value) {
+        router.push("/");
+      }
+    });
+  },
 };
 </script>
 
