8e0f77ddcb4742573d526e65f659873da1eafe12
zawthuphyo <ztphyo101@gmail.com>
2025-04-11 23:21:45 +0700

    route guard and wait initailize vue app for connect to auth system

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Chatroom from "@/views/Chatroom.vue";
 import Welcome from "@/views/Welcome.vue";
 import { createRouter, createWebHistory } from "vue-router";
+import { auth } from "@/firebase/config";
 
 const routes = [
   {
@@ -12,6 +13,14 @@ const routes = [
     path: "/chatroom",
     name: "Chatroom",
     component: Chatroom,
+    beforeEnter: (to, from, next) => {
+      let user = auth.currentUser;
+      if (user) {
+        next();
+      } else {
+        next("/");
+      }
+    },
   },
 ];
 
