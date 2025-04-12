dd1638d8552a52bfda3ba1bde25e56623a37522e
zawthuphyo <ztphyo101@gmail.com>
2025-04-12 20:05:24 +0700

    real time firestore listener

--- src/components/ChatWindow.vue
+++ src/components/ChatWindow.vue
@@ -1,17 +1,45 @@
 <template>
   <div class="chat-window">
-    <div class="messages">
+    <div class="messages" v-for="message in messages" :key="message.id">
       <div class="singal">
-        <span class="created-at">3min ago</span>
-        <span class="name">Zaw Zaw</span>
-        <span class="message">Hello, how can I help you?</span>
+        <span class="created-at">{{ message.created_at }}</span>
+        <span class="name">{{ message.name }}</span>
+        <span class="message">{{ message.message }}</span>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+import { db } from "@/firebase/config";
+import { ref } from "vue";
+
+export default {
+  setup() {
+    let messages = ref([]);
+    db.collection("messages")
+      .orderBy("created_at")
+      .onSnapshot((snap) => {
+        // console.log(snap);
+        let results = [];
+        snap.docs.forEach((doc) => {
+          //   console.log(doc.data());
+          let document = {
+            id: doc.id,
+            ...doc.data(),
+          };
+          //   console.log(document);
+          results.push(document);
+          //   console.log(results);
+        });
+        messages.value = results;
+        console.log(messages.value);
+      });
+    return {
+      messages,
+    };
+  },
+};
 </script>
 
 <style>
